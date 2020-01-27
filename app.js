b99343e883ed8d5551620286fec8530466617a3f
Apurva <apurva.gurme@thoughtworks.com>
2020-01-27 21:31:41 +0530

    Refactored code

--- app.js
+++ app.js
@@ -1,39 +1,8 @@
 const STATIC_FOLDER = `${__dirname}/public`;
 const TEMPLATE_FOLDER = `${__dirname}/templates`;
 const Response = require('./lib/response');
+const CONTENT_TYPES = require('./lib/contentType');
 const fs = require('fs');
-const CONTENT_TYPES = {
-  txt: 'text/plain',
-  html: 'text/html',
-  css: 'text/css',
-  js: 'application/javascript',
-  json: 'application/json',
-  gif: 'image/gif',
-  jpg: 'image/jpeg'
-};
-
-const serveHomePage = function(req) {
-  req.url = '/index.html';
-  const res = serveStaticPage(req);
-  return res;
-};
-
-const serveStaticPage = function(req) {
-  req.url === '/guestBook.html'
-    ? (path = `${TEMPLATE_FOLDER}${req.url}`)
-    : (path = `${STATIC_FOLDER}${req.url}`);
-  const status = fs.existsSync(path) && fs.statSync(path);
-  if (!status || !status.isFile) return new Response();
-  const content = fs.readFileSync(path);
-  const [, extension] = req.url.split('.');
-  const contentType = CONTENT_TYPES[extension];
-  const res = new Response();
-  res.setHeader('Content-Type', contentType);
-  res.setHeader('Content-Length', content.length);
-  res.statusCode = 200;
-  res.body = content;
-  return res;
-};
 
 const storeRecord = function(keyAndValue, records) {
   content = JSON.parse(records);
@@ -56,6 +25,15 @@ const createTable = function(records) {
   return table;
 };
 
+const getResponse = function({ contentType, body }) {
+  const res = new Response();
+  res.setHeader('Content-Type', contentType);
+  res.setHeader('Content-Length', body.length);
+  res.statusCode = 200;
+  res.body = body;
+  return res;
+};
+
 const saveComment = function(req) {
   let records = fs.readFileSync('./commentRecords.json', 'utf8');
   storeRecord(req.body, records);
@@ -64,16 +42,33 @@ const saveComment = function(req) {
   let content = fs.readFileSync(path, 'utf8');
   const table = createTable(JSON.parse(records));
   const replaced = content.replace('__COMMENT-LIST__', table);
-  const res = new Response();
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Length', replaced.length);
-  res.statusCode = 200;
-  res.body = replaced;
+  const contentType = 'text/html';
+  const res = getResponse({ contentType, body: replaced });
+  return res;
+};
+
+const serveStaticPage = function(req) {
+  req.url === '/guestBook.html'
+    ? (path = `${TEMPLATE_FOLDER}${req.url}`)
+    : (path = `${STATIC_FOLDER}${req.url}`);
+  const status = fs.existsSync(path) && fs.statSync(path);
+  if (!status || !status.isFile) return new Response();
+  let content = fs.readFileSync(path);
+  const [, extension] = req.url.split('.');
+  const contentType = CONTENT_TYPES[extension];
+  const res = getResponse({ contentType, body: content });
+  return res;
+};
+
+const serveHomePage = function(req) {
+  req.url = '/index.html';
+  const res = serveStaticPage(req);
   return res;
 };
 
 const findHandler = function(req) {
   if (req.method === 'GET' && req.url === '/') return serveHomePage;
+  if (req.method === 'GET' && req.url === '/guestBook.html') return serveStaticPage;
   if (req.method === 'GET') return serveStaticPage;
   if (req.method === 'POST' && req.url === '/addComment') return saveComment;
   return () => new Response();
