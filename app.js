24ba114094558bfba13c859ec2a66bdfde1623a9
Apurva <apurva.gurme@thoughtworks.com>
2020-01-27 19:29:18 +0530

    Added feature that is giving whole date, name and comment list

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const serveHomePage = function(req) {
   const res = serveStaticPage(req);
   return res;
 };
+
 const serveStaticPage = function(req) {
   const path = `${STATIC_FOLDER}${req.url}`;
   const status = fs.existsSync(path) && fs.statSync(path);
@@ -35,14 +36,11 @@ const serveStaticPage = function(req) {
 const serveGuestBook = function(req) {
   const path = `${TEMPLATE_FOLDER}${req.url}`;
   const status = fs.existsSync(path) && fs.statSync(path);
-
   if (!status || !status.isFile) return new Response();
   let content = fs.readFileSync(path);
-
   const [, extension] = req.url.split('.');
   const contentType = CONTENT_TYPES[extension];
   const res = new Response();
-
   res.setHeader('Content-Type', contentType);
   res.setHeader('Content-Length', content.length);
   res.statusCode = 200;
@@ -50,20 +48,38 @@ const serveGuestBook = function(req) {
   return res;
 };
 
+const storeRecord = function(keyAndValue, records) {
+  content = JSON.parse(records);
+  content.unshift(keyAndValue);
+  content = JSON.stringify(content);
+  fs.writeFileSync('./commentRecords.json', content);
+};
+
+const reducer = function(container, record) {
+  const row = `<tr>
+    <td>${new Date().toLocaleString()}</td>
+    <td>${record.name}</td>
+    <td>${record.comment}</td>
+    </tr>`;
+  return container.concat(row);
+};
+
+const createTable = function(records) {
+  const table = records.reduce(reducer, '');
+  return table;
+};
+
 const saveComment = function(req) {
+  let records = fs.readFileSync('./commentRecords.json', 'utf8');
+  storeRecord(req.body, records);
+  records = fs.readFileSync('./commentRecords.json', 'utf8');
   const path = `${TEMPLATE_FOLDER}/guestBook.html`;
   let content = fs.readFileSync(path, 'utf8');
-  const replaced = content.replace(
-    '__COMMENT-LIST__',
-    `<td>${new Date().toLocaleString()}</td><td>${req.body.name}</td><td>${
-      req.body.comment
-    }</td></tr>`
-  );
-  const [, extension] = req.url.split('.');
-  const contentType = CONTENT_TYPES[extension];
+  const table = createTable(JSON.parse(records));
+  const replaced = content.replace('__COMMENT-LIST__', table);
   const res = new Response();
-  res.setHeader('Content-Type', contentType);
-  res.setHeader('Content-Length', content.length);
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', replaced.length);
   res.statusCode = 200;
   res.body = replaced;
   return res;
